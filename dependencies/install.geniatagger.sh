bb7525f9fdeb20b9c324ede2285c656d6210d2dd
Erwan Moreau <moreaue@cs.tcd.ie>
2021-01-22 16:59:37 +0000

    fixed GeniaTagger broken path and replaced with new version number

--- dependencies/install.geniatagger.sh
+++ dependencies/install.geniatagger.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 set -ex
 
-rm -fr geniatagger-python-0.1 geniatagger-3.0.1
+# EM Jan 21 - fixed path and version no
+
+rm -fr geniatagger-python-0.1 geniatagger-3.0.2
 
 # First let's install GeniaTagger
+# wget http://www.nactem.ac.uk/tsujii/GENIA/tagger/geniatagger-3.0.1.tar.gz
+wget http://www.nactem.ac.uk/GENIA/tagger/geniatagger-3.0.2.tar.gz
 
-wget http://www.nactem.ac.uk/tsujii/GENIA/tagger/geniatagger-3.0.1.tar.gz
+tar xvf geniatagger-3.0.2.tar.gz
 
-tar xvf geniatagger-3.0.1.tar.gz
-
-cd geniatagger-3.0.1
+cd geniatagger-3.0.2
 
 # Fix a missing header
 echo "#include <cstdlib>" > morph2.cpp
@@ -20,7 +22,7 @@ make
 
 cd -
 
-rm geniatagger-3.0.1.tar.gz
+rm geniatagger-3.0.2.tar.gz
 
 # Then let's install a Python wrapper for it
 wget https://pypi.python.org/packages/cf/19/f61aa1318ca440a834ce433c49832047919a9665c421a633fe32896455fb/geniatagger-python-0.1.tar.gz
